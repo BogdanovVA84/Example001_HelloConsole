030daf00fcbe1fc30438555307e2b150ee9d6457
Vasilii <bogdanovva84@mail.ru>
2022-06-28 01:11:46 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,24 @@
+﻿//вариант 1
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 11, 12, 31, 41, 15, 61, 19, 18, 19 };
+
+        int n = array.Length;
+        int find = 19;
+        int index = 0;
+        while(index < n)
+        {
+            if(array[index] == find)
+            {
+                Console.WriteLine(index);
+                break; //без этого оператора прога будет выдовать несколько занчений
+            }
+            //index = index + 1;
+            index++;
+        }
+    }
+}
+
+
