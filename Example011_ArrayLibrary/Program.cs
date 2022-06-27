2881f964370494b77ae1d31cc5ec6fa6b203889b
Vasilii <bogdanovva84@mail.ru>
2022-06-28 01:43:58 +0300

    Example011_ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,4 +1,42 @@
-﻿internal partial class Program
+﻿//Version 1
+// internal partial class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         void FillArray(int[] collection)
+//         {
+//             int Length = collection.Length;
+//             int index = 0;
+//             while (index < Length)
+//             {
+//                 collection[index] = new Random().Next(1, 10);
+//                 index++;
+
+//             }
+//         }       
+        
+//         void PrintArray(int[] col)
+//         {
+//             int count = col.Length;
+//             int position = 0;
+//             while (position < count)
+//             {
+
+//                 Console.WriteLine(col[position]);
+//                 position++;
+//             }
+
+//         }
+
+//         int[] array = new int [10];
+
+//         FillArray(array);
+//         PrintArray(array);
+//     }
+// }
+
+//Version 2
+internal partial class Program
 {
     private static void Main(string[] args)
     {
@@ -27,9 +65,35 @@
 
         }
 
+        int indexOf(int[] collection, int find)
+        {
+            int count = collection.Length;
+            int index = 0;
+            int position = -1; //Хитрый ход поставить -1. Чтобы был вывод, что такой элемент не найден.
+
+            while (index < count)
+            {
+                if(collection[index] ==find)
+                {
+                    position = index;
+                    break; //Для вывода только одного первого значения при повторении.
+                }
+                index++;
+            }
+            return position;
+        }
         int[] array = new int [10];
 
         FillArray(array);
+        array[4] = 4;
+        array[6] = 4;
+
         PrintArray(array);
-    }
+        Console.WriteLine();
+
+        int pos = indexOf(array, 444);
+        Console.WriteLine(pos);
+
+    }   
+
 }
