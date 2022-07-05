cc191d3a384a95af5525ec0fc53a08301ae0b6c3
Vasilii <bogdanovva84@mail.ru>
2022-07-05 21:49:21 +0300

    обновлен Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -35,18 +35,81 @@ int Method3()
 // Console.WriteLine(year);
 
 // Вид 4 - что-то принимают, что-то возвращают
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+
 string Method4(int count, string text)
 {
-    int i = 0;
+    
     string result = String.Empty;
-
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
-        result = result + text;
-        i++;
+        result = result + text;        
     }
     return result;
 }
 
 string res = Method4(10, "z");
-Console.WriteLine(res);
+// Console.WriteLine(res);
+
+// //Таблица умножения
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+// маленькими “с”.
+
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + " вы бы взяли приступом согласие прусского короля."
+            + " Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//            012
+// s [3] // r 
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
