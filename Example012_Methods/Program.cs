28209835f7a0edebe309ba660a16496b3a2d10ba
Vasilii <bogdanovva84@mail.ru>
2022-07-05 21:09:08 +0300

    обновлен Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,23 +1,52 @@
-﻿// Вид 1
+﻿// Вид 1 - ничего не принимают, ничего не возвращают
 void Method1()
 {
     Console.WriteLine("Автор ...");
-
 }
-Method1();
+// Method1();
 
-// Вид 2
-void Method2 (string msg)
+// Вид 2 - что-то принимают, ничего не возвращают
+void Method2(string msg)
 {
     Console.WriteLine(msg);
 }
-Method2(msg: "Текст сообщения");
+// Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
 {
     int i = 0;
-    while(i<count)
+    while(i < count)
     {
         Console.WriteLine(msg);
         i++;
     }
 }       
-Method21(msg: "Текст", count: 4);
+// Method21(msg: "Текст", count: 4);
+// Method21(count: 5, msg: "новый текст");
+
+// Вид 3 - ничего не принимают, что-то возвращают
+int Method3()
+{
+    return DateTime.Now.Year;
+
+}
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// Вид 4 - что-то принимают, что-то возвращают
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
